57d6170c598e8a5c4dddc53c78cd2a290aa74301
Francisco <cinkii@protonmail.com>
2024-08-08 12:16:33 -0300

    🎉

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <main className="container">
+      <section className="header">
+        <div>
+          <h1>Announce React</h1>
+          <p>
+            Best banner library for React projects
+          </p>
+        </div>
+      </section>
+      <section className="hero">
+        
+      </section>
+    </main>
+  )
+}
+
+export default App
